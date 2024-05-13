29aff5e7e6d75dc317da0c887183eee92886ae6a
841973620 <841973620@qq.com>
2024-05-14 02:05:59 +0800

    fix ubuntu-docker.sh

--- ubuntu-docker.sh
+++ ubuntu-docker.sh
@@ -47,8 +47,9 @@ if [ $DGX_REPO == true ]; then
 			apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 7EA0A9C3F273FCD8
 		elif [[ $VERSION_ID = 14.04 ]]; then
 			curl -fkSLO https://international.download.nvidia.com/dgx1/repos/pool/multiverse/d/dgx1-repo/dgx1-repo-ubuntu1404_1.1-1_amd64.deb
-			dpkg -i dgx1-repo-ubuntu1404_1.1-1_amd64.deb
-			rm dgx1-repo-ubuntu1404_1.1-1_amd64.deb
+			curl -fkSLO https://repo.download.nvidia.com/dgx/repos/bionic/pool/multiverse/d/dgx-repo-keys/dgx-repo-keys_2.0_amd64.deb
+			dpkg -i dgx1-repo-ubuntu1404_1.1-1_amd64.deb dgx-repo-keys_2.0_amd64.deb
+			rm dgx1-repo-ubuntu1404_1.1-1_amd64.deb dgx-repo-keys_2.0_amd64.deb
 		else
 			echo "Error: unsupported ubuntu version."
 		fi
@@ -59,37 +60,41 @@ if [ $NO_INSTALL_DOCKER == true ]; then
 	exit
 fi
 
-reqs="docker-ce docker-ce-cli containerd.io"
-
-if cat /etc/apt/sources.list.d/dgx.list > /dev/null; then
-	reqs="$reqs dgx-docker-cleanup dgx-docker-options"
-	
-	if [[ $UBUNTU_CODENAME = jammy ]] || [[ $UBUNTU_CODENAME = focal ]]; then
-		reqs="$reqs nvidia-container-toolkit"
-	elif [[ $UBUNTU_CODENAME = bionic ]] || [[ $UBUNTU_CODENAME = xenial ]] || [[ $UBUNTU_CODENAME = trusty ]]; then
-		reqs="$reqs nvidia-docker2"
-	fi
+if [[ $VERSION_ID = 14.04 ]]; then
+	reqs="docker.io"
 else
-	pre_reqs="apt-transport-https ca-certificates"
+	reqs="docker-ce docker-ce-cli containerd.io"
 	
-	if ! command -v gnupg > /dev/null; then
-		pre_reqs="$pre_reqs gnupg"
-	fi
-	
-	if [[ $UBUNTU_CODENAME = jammy ]] || [[ $UBUNTU_CODENAME = focal ]]; then
-		reqs="$reqs docker-buildx-plugin docker-compose-plugin nvidia-container-toolkit"
+	if cat /etc/apt/sources.list.d/dgx.list > /dev/null; then
+		reqs="$reqs dgx-docker-cleanup dgx-docker-options"
+		
+		if [[ $UBUNTU_CODENAME = jammy ]] || [[ $UBUNTU_CODENAME = focal ]]; then
+			reqs="$reqs nvidia-container-toolkit"
+		elif [[ $UBUNTU_CODENAME = bionic ]] || [[ $UBUNTU_CODENAME = xenial ]]; then
+			reqs="$reqs nvidia-docker2"
+		fi
 	else
-		reqs="$reqs nvidia-container-toolkit"
-	fi
+		pre_reqs="apt-transport-https ca-certificates"
+		
+		if ! command -v gnupg > /dev/null; then
+			pre_reqs="$pre_reqs gnupg"
+		fi
+		
+		if [[ $UBUNTU_CODENAME = jammy ]] || [[ $UBUNTU_CODENAME = focal ]]; then
+			reqs="$reqs docker-buildx-plugin docker-compose-plugin nvidia-container-toolkit"
+		else
+			reqs="$reqs nvidia-container-toolkit"
+		fi
 	
-	apt update
-	DEBIAN_FRONTEND=noninteractive apt install -y $pre_reqs
-	install -m 0755 -d /etc/apt/keyrings
-	echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-	curl -fkSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-	chmod a+r /etc/apt/keyrings/docker.gpg
-	curl -ksL https://nvidia.github.io/nvidia-docker/gpgkey | apt-key add -
-	curl -ksL https://nvidia.github.io/nvidia-docker/$(. /etc/os-release;echo $ID$VERSION_ID)/nvidia-docker.list | tee /etc/apt/sources.list.d/nvidia-docker.list
+		apt update
+		DEBIAN_FRONTEND=noninteractive apt install -y $pre_reqs
+		install -m 0755 -d /etc/apt/keyrings
+		echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+		curl -fkSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+		chmod a+r /etc/apt/keyrings/docker.gpg
+		curl -ksL https://nvidia.github.io/nvidia-docker/gpgkey | apt-key add -
+		curl -ksL https://nvidia.github.io/nvidia-docker/$(. /etc/os-release;echo $ID$VERSION_ID)/nvidia-docker.list | tee /etc/apt/sources.list.d/nvidia-docker.list
+	fi
 fi
 
 apt update
